c3da5941a0f948b3593440962668c11b57e8feb9
hyandmyf <1796284457@qq.com>
2018-12-11 16:43:27 +0800

    11111

---
+++ BWLApplication/app/src/main/java/bdqn/cn/bwlapplication/util/DataFormatType.java
@@ -0,0 +1,25 @@
+package bdqn.cn.bwlapplication.util;
+
+public enum  DataFormatType {
+
+    /**
+     * MM-dd HH:mm
+     */
+    REMOVE_YEAR_TIME,
+    /**
+     * yyyy-MM-dd HH:mm
+     */
+    NORMAL_TIME,
+    /**
+     * MM-dd
+     */
+    REMOVE_YEAR_DATE,
+    /**
+     * yyyy-MM-dd
+     */
+    NORMAL_DATE,
+    /**
+     * yyyy-MM-dd-HH-mm
+     */
+    SPECIAL_TYPE
+}
